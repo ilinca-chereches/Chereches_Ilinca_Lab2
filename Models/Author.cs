70938b0edf6346056528a00e04a497712f6d1864
ilinca-chereches <ilinca.chereches@stud.ubbcluj.ro>
2025-10-11 12:24:07 +0300

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,19 @@
+﻿namespace Chereches_Ilinca_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+
+        
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<Book>? Books { get; set; }
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+    }
+}
